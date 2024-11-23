e281b5c31399bb5e10fe66879f8ffd8e6183a790
Fábio Rodrigues de Moraes <fmoraes05@outlook.com>
2024-11-23 12:19:22 -0300

    Trabalhando com páginas html

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -2,4 +2,22 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-    return render(request, "home_page.html",{})
+    context = {
+        "title": "Página Principal",
+        "content": "Bem-vindo à página Principal...",
+    }
+    return render(request, "home_page.html", context)
+
+def about_page(request):
+    context = {
+        "title": "Página About",
+        "content": "Bem-vindo à Página About...",
+    }
+    return render(request, "about/view.html", context)
+
+def contact_page(request):
+    context = {
+        "title": "Página Contact",
+        "content": "Bem-vindo à Página Contact...",
+    }
+    return render(request, "contact/view.html", context)

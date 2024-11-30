b086218a35a3e862f1a1964d91e7529d0c1dc2ef
Fábio Rodrigues de Moraes <fmoraes05@outlook.com>
2024-11-29 21:31:22 -0300

    CBV e FBV

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
+from django.views.generic import ListView
 from django.shortcuts import render
+from .models import Products
 
-# Create your views here.
+#Class Based Views
+class ProductListView(ListView):
+    queryset = Products.objects.all()
+    template_name= "products/list.html"
+
+#Function Based Views
+def product_list_view(request):
+    queryset = Products.objects.all()
+    context = {
+        'object_list': queryset,
+    }
+
+    return render(request,"products/list.html", context)

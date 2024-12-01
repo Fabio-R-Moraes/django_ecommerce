9d4ef1cb93a03a624f704967ea0cc72d6ff870ec
Fábio Rodrigues de Moraes <fmoraes05@outlook.com>
2024-12-01 20:30:58 -0300

    Lookups

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
 from .models import Products
@@ -29,7 +30,14 @@ class ProductDetailView(DetailView):
 #Function Based Views
 def product_detail_view(request, pk=None, *args, **kwargs):
     #instance = Products.objects.get(pk=pk)
-    instance = get_object_or_404(Products, pk=pk)
+    #instance = get_object_or_404(Products, pk=pk)
+    qs = Products.objects.filter(id=pk)
+
+    if qs.count() == 1:
+        instance = qs.first()
+    else:
+        raise Http404("Esse produto NÃO está cadastrado!!!")
+    
     context = {
         'object': instance,
     }

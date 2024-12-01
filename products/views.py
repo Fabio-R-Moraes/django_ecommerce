a3d47378b67197840f812d2f57a89a177a8a7398
Fábio Rodrigues de Moraes <fmoraes05@outlook.com>
2024-12-01 11:23:48 -0300

    DetailView - CBV e FBV

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import ListView
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
 from .models import Products
 
 #Class Based Views
@@ -15,3 +15,23 @@ def product_list_view(request):
     }
 
     return render(request,"products/list.html", context)
+
+#Class Based Views
+class ProductDetailView(DetailView):
+    queryset = Products.objects.all()
+    template_name= "products/detail.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+
+#Function Based Views
+def product_detail_view(request, pk=None, *args, **kwargs):
+    #instance = Products.objects.get(pk=pk)
+    instance = get_object_or_404(Products, pk=pk)
+    context = {
+        'object': instance,
+    }
+
+    return render(request, "products/detail.html", context)

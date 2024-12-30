ed50fb342cec13b1cf7f9c39dbcf2d9a4edd5a87
Fábio Rodrigues de Moraes <fmoraes05@outlook.com>
2024-12-30 17:02:37 -0300

    Gerando ID do Pedido

--- e_commerce/utils.py
+++ e_commerce/utils.py
@@ -7,6 +7,20 @@ from django.utils.text import slugify
 def random_string_generator(size = 10, chars = string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
+def unique_order_id_generator(instance):
+    """
+    Isso é para o projeto Django e-commerce com o campo order_id
+    """
+    order_new_id = random_string_generator()
+    Klass = instance.__class__
+    qs_exists = Klass.objects.filter(order_id=order_new_id).exists()
+
+    if qs_exists:
+        return unique_order_id_generator(instance)
+    
+    return order_new_id
+    
+
 
 def unique_slug_generator(instance, new_slug=None):
     """
--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from products.utils import unique_slug_generator
+from e_commerce.utils import unique_slug_generator
 from django.db.models.signals import pre_save
 from django.urls import reverse
 from django.db.models import Q

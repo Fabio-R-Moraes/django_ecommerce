c4a9b04791b290e99306dd7bfaea28f185d1daa2
Fábio Rodrigues de Moraes <fmoraes05@outlook.com>
2025-01-30 19:16:58 -0300

    Botão Adicionar na lista de produtos

--- products/views.py
+++ products/views.py
@@ -19,6 +19,13 @@ class ProductListView(ListView):
     queryset = Products.objects.all()
     template_name= "products/list.html"
 
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        cart_obj, new_obj = Cart.objects.new_or_get(self.request)
+        context['cart'] = cart_obj
+
+        return context
+
 #Function Based Views
 def product_list_view(request):
     queryset = Products.objects.all()
